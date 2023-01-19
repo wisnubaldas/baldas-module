705ab5eba2df959d69ae9583e469fe0d6cdbe002
Wisnu Hidayat <wisnubaldas@gmail.com>
2023-01-19 19:02:23 +0700

    fix in win 11

--- src/App/Controllers/Controller.php
+++ src/App/Controllers/Controller.php
@@ -9,4 +9,26 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
      use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+     
+     public function handleResponse($result, $msg)
+    {
+    	$res = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $msg,
+        ];
+        return response()->json($res, 200);
+    }
+
+    public function handleError($error, $errorMsg = [], $code = 404)
+    {
+    	$res = [
+            'success' => false,
+            'message' => $error,
+        ];
+        if(!empty($errorMsg)){
+            $res['data'] = $errorMsg;
+        }
+        return response()->json($res, $code);
+    }
 }
--- src/routes/auth.php
+++ src/routes/auth.php
@@ -2,13 +2,22 @@
 use Illuminate\Support\Facades\Route;
 use Wisnubaldas\BaldasModule\App\Controllers\AuthController;
 
-
-Route::prefix('auth')->group(function () {
-	Route::post('signup', [AuthController::class,'signup'])->name('auth.signup');
-	Route::match(['get', 'post'],'login', [AuthController::class,'login'])->name('login');
-	Route::post('logout', [AuthController::class,'logout'])->middleware('auth:sanctum')->name('auth.logout');
-	Route::get('user', [AuthController::class,'getAuthenticatedUser'])->middleware('auth:sanctum')->name('auth.user');
-
-	Route::post('/password/email', [AuthController::class,'sendPasswordResetLinkEmail'])->middleware('throttle:5,1')->name('password.email');
-	Route::post('/password/reset', [AuthController::class,'resetPassword'])->name('password.reset');
+Route::middleware('auth:sanctum')->group( function () {
+   Route::post('logout',[AuthController::class, 'logout']);
+   Route::get('user', function(){
+		return Auth::user();
+   });
 });
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+// Route::prefix('auth')->group(function () {
+// 	Route::post('signup', [AuthController::class,'signup'])->name('auth.signup');
+// 	Route::match(['get', 'post'],'login', [AuthController::class,'login'])->name('login');
+// 	Route::post('logout', [AuthController::class,'logout'])->middleware('auth:sanctum')->name('auth.logout');
+// 	Route::get('user', [AuthController::class,'getAuthenticatedUser'])->middleware('auth:sanctum')->name('auth.user');
+
+// 	Route::post('/password/email', [AuthController::class,'sendPasswordResetLinkEmail'])->middleware('throttle:5,1')->name('password.email');
+// 	Route::post('/password/reset', [AuthController::class,'resetPassword'])->name('password.reset');
+// });
